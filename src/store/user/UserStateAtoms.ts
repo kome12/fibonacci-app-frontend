2a57abbbc62974b52c6b1eacb00e06bd03207d11
Daichi Hashimura <dhashi92@gmail.com>
2021-08-26 03:12:44 +0900

    Setup recoil to handle userData

---
+++ src/store/user/UserStateAtoms.ts
@@ -0,0 +1,12 @@
+import { atom } from 'recoil'
+
+type UserData = {
+  id: string
+  displayName: string
+  imageUrl: string | null
+}
+
+export const userState = atom<UserData | undefined>({
+  key: 'userData',
+  default: undefined,
+})
