ffae6932aab8c30580f5a0af749a06e34afde18e
Conall Imlah <conallimlah@gmail.com>
2021-08-25 21:41:15 +0900

    update component folder structure

---
+++ src/pages/GardenView/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+
+export const GardenView = ()  => {
+  return (
+    <div>
+      <Header />
+      <h2>Garden View</h2>
+    </div>
+  )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+
+export const Home = ()  => {
+  return (
+    <div>
+      <Header />
+      <h2>Home</h2>
+    </div>
+  )
+}
