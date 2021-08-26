416144ff8b90207135383700fe8f62e590719663
Daichi Hashimura <dhashi92@gmail.com>
2021-08-26 23:37:52 +0900

    Add axios config

---
+++ src/utils/api/index.ts
@@ -0,0 +1,67 @@
+import axios, { AxiosError, AxiosRequestConfig } from 'axios'
+
+export const baseURL = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'https://the-fibonacci-api.herokuapp.com/'
+    case 'test':
+      return 'https://the-fibonacci-api-staging.herokuapp.com/api/v1'
+    case 'development':
+      return 'http://localhost:3001/api/v1'
+  }
+}
+
+export const clientBaseURL = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'https://the-fibonacci-app.herokuapp.com/'
+    case 'test':
+      return 'https://the-fibonacci-app-staging.herokuapp.com/'
+    case 'development':
+      return 'http://localhost:3000'
+  }
+}
+
+export type ApiResponse<T> =
+  | {
+      error: undefined
+      data: T
+    }
+  | {
+      error: AxiosError
+      data: undefined
+    }
+
+async function request<T>(config: AxiosRequestConfig): Promise<ApiResponse<T>> {
+  try {
+    const res = await axios.request<T>({
+      timeout: 10000,
+      baseURL: baseURL(),
+      // xsrfHeaderName: 'X-CSRF-Token',
+      // withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        // 'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': clientBaseURL(),
+      },
+      responseType: 'json',
+      ...config,
+    })
+    return { error: undefined, data: res.data }
+  } catch (error) {
+    return { error, data: undefined }
+  }
+}
+
+export const api = {
+  get<R>(url: string, options?: AxiosRequestConfig) {
+    return request<R>({ method: 'get', url, ...options })
+  },
+  post<R>(url: string, data?: unknown) {
+    return request<R>({ method: 'post', url, data })
+  },
+  put<R>(url: string, data?: unknown) {
+    return request<R>({ method: 'put', url, data })
+  },
+}
