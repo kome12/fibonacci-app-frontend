6c1390a65f06ae4becdf87f0677fb47b0802ceea
Daichi Hashimura <dhashi92@gmail.com>
2021-08-26 23:38:21 +0900

    Add sample code of how to use useApi

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useMemo } from 'react'
 import { SignIn } from '../../components/SignIn'
+import { getGardens } from '../../helpers/api/gardens/getGardens'
 import { useUserState } from '../../store/user/useUserState'
+import { useApi } from '../../utils/api/useApi'
 
 export const Home = () => {
   const { userData } = useUserState()
+
+  // Sample code of how to use the useApi hook in a component
+  // If you want to check useApi: src/utils/api/useApi.ts
+  // How to define the apiHelper(getGardens): src/helpers/api/gardens/getGardens.ts
+  const [gardensApi, getGardensData] = useApi(getGardens)
+  const gardens = useMemo(() => gardensApi.response ?? [], [gardensApi])
+  console.log({ gardens })
+  useEffect(() => {
+    getGardensData()
+  }, [])
 
   return (
     <div>
