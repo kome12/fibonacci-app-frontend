66f1dca577e2f2e99ff26443b6aa6432f0ef3656
Conall Imlah <conallimlah@gmail.com>
2021-08-27 20:13:58 +0900

    move rules to AddRules component

--- src/pages/CreateGarden/index.tsx
+++ src/pages/CreateGarden/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { styled } from '@material-ui/core/styles';
 import { Button, Container, MobileStepper } from "@material-ui/core";
-
-import { UserRule } from "./components/UserRule";
+import { AddRules } from "./components/AddRules";
 import { NewGarden } from "./components/NewGarden";
 import "./CreateGardenPage.css";
 
@@ -21,13 +20,7 @@ const CreateGardenStepper = styled(MobileStepper)({
   borderRadius: 20,
 })
 
-export interface NewUserRule {
-  name: String,
-  description?: String,
-}
-
 export const CreateGarden = ()  => {
-  const [userRules, setUserRules] = useState<NewUserRule[]>([]);
   const [activeStep, setActiveStep] = useState(0);
 
   const handleNext = () => {
@@ -43,8 +36,7 @@ export const CreateGarden = ()  => {
       <CreateGardenContainer>
         <div className="create-garden">
           {activeStep === 0 && <NewGarden />}
-          {activeStep === 1 && userRules.length < 1 && <div><h3>Current rules:</h3><p>There are currently no rules set for this garden.</p></div>}
-          {userRules.map((rule, idx) => <UserRule key={idx} name={rule.name} description={rule.description}/>)}
+          {activeStep === 1 && <AddRules />}
         </div>
       <CreateGardenStepper
         variant="progress"
