8b92e35a80a35abb0e325421dc1e4a472819ab0b
kome12 <ko.take5481@gmail.com>
2021-08-28 00:42:02 +0900

    Connected rules to staging API - talked with Daichi regarding hardcoded API

--- src/helpers/api/gardens/getGardens.ts
+++ src/helpers/api/gardens/getGardens.ts
@@ -1,4 +1,9 @@
-import { api } from '../../../utils/api'
+import { PopulatedGarden } from "../../../models/garden.model";
+import { api } from "../../../utils/api";
 
 export const getGardens = () =>
-  api.get<[{ id: string; name: string }]>('/gardens')
+  api.get<[{ id: string; name: string }]>("/gardens");
+
+export const getGardenByGardenId = (
+  gardenId: string = "612789b765c2e6a7e7e76bd2"
+) => api.get<PopulatedGarden>(`/gardens/${gardenId}`);
---
+++ src/models/rule.model.tsx
@@ -0,0 +1,5 @@
+export interface Rule {
+  _id: string;
+  name: string;
+  description: string;
+}
