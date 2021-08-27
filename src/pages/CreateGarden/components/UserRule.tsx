2603bf755ddc1e89b37dd5a229f86da2b2642347
Conall Imlah <conallimlah@gmail.com>
2021-08-27 20:14:25 +0900

    adjust UserRule component based on AddRules component

--- src/pages/CreateGarden/components/UserRule.tsx
+++ src/pages/CreateGarden/components/UserRule.tsx
@@ -1,9 +1,10 @@
 import { Box } from "@material-ui/core";
-import { NewUserRule } from "..";
+import { NewUserRule } from "./AddRules";
+import "./UserRule.css";
 
 export const UserRule = ({ name, description }: NewUserRule) => {
   return (
-    <Box color="secondary">
+    <Box>
       <h3>{name}</h3>
       {description && <p>{description}</p>}
     </Box>
