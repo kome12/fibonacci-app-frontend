81e14ada1564853f7c75023a63a097180b1a8a99
Conall Imlah <conallimlah@gmail.com>
2021-08-27 17:54:35 +0900

    add input description, fix styling

--- src/pages/CreateGarden/components/NewGarden.tsx
+++ src/pages/CreateGarden/components/NewGarden.tsx
@@ -14,9 +14,11 @@ export const NewGarden = () => {
     <div className="new-garden-container">
       <h2>Create Garden</h2>
         <label htmlFor="desc"><p>Name:</p></label>
+        <p>Give your Garden a name! *required</p>
         <input className="garden-name" type="text" name="name" onChange={nameChangeHandler} value={name}/>
 
         <label htmlFor="desc"><p>Description:</p></label>
+        <p>Add a description! (optional)</p>
         <input className="garden-desc" type="text" name="desc" onChange={descChangeHandler} value={desc} />
     </div>
   )
