0bbbd3e2b04da8ad66fcd285e4011f650507003f
Daichi Hashimura <dhashi92@gmail.com>
2021-09-02 00:48:51 +0900

    Use Loading component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { AppWrapper } from "./components/AppWrapper";
+import { Loading } from "./components/LoadingWrapper/Loading";
 import { Routes } from "./routes";
 import { MyNiwaTheme } from "./utils/UITheme";
 
-const Loading = () => <div>Page Loading...</div>;
 const App = () => {
   return (
     <div>
       <RecoilRoot>
         <AppWrapper>
           <Router>
-           <ThemeProvider theme={MyNiwaTheme}>
+            <ThemeProvider theme={MyNiwaTheme}>
               <Suspense fallback={<Loading />}>
                 <Routes />
               </Suspense>
