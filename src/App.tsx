f43b98b72fbcdff4619ed8c96e7611e450846efa
Conall Imlah <conallimlah@gmail.com>
2021-09-01 16:24:50 +0900

    add palette to ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { ThemeProvider } from "@material-ui/core";
 import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Header } from "./components/Header";
 import { Routes } from "./routes";
+import { MyNiwaTheme } from "./utils/UITheme";
+
 const Loading = () => <div>Page Loading...</div>;
 const App = () => {
   return (
     <div>
       <RecoilRoot>
         <Router>
+          <ThemeProvider theme={MyNiwaTheme}>
           <Header />
           <Suspense fallback={<Loading />}>
             <Routes />
           </Suspense>
+          </ThemeProvider>
         </Router>
       </RecoilRoot>
     </div>
