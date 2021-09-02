2b31af5f3158aabfa52fce2a247f26910bf054b6
Conall Imlah <conallimlah@gmail.com>
2021-09-02 16:33:13 +0900

    add more Material components, remove css

--- src/pages/CreateGarden/components/NewGarden.tsx
+++ src/pages/CreateGarden/components/NewGarden.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@material-ui/core";
+import { Grid, TextField, Typography } from "@material-ui/core";
 import { motion } from "framer-motion";
 import React from "react";
 import "./NewGarden.css";
@@ -21,39 +21,40 @@ export const NewGarden: React.FC<NewGardenProps> = ({
   const initDir = animDirection === "left" ? "5vw" : "-5vw";
   const exitDir = animDirection === "left" ? "-5vw" : "5vw";
   return (
-    <Container
+    <Grid
+      container={true}
       className="new-garden-container"
+      justifyContent="center"
       component={motion.div}
       initial={{ opacity: 0, x: initDir }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.6 }}
       exit={{ opacity: 0, x: exitDir }}
     >
-      <h2>Create Garden</h2>
-      <label htmlFor="desc">
-        <p>Name:</p>
-      </label>
-      <p>Give your Garden a name! *required</p>
-      <input
-        className="garden-name"
+      <Typography variant="h3">Create Garden</Typography>
+      <Typography variant="overline">Give your Flower Bed a name.</Typography>
+      <TextField
         type="text"
         name="name"
         onChange={nameChangeHandler}
         value={name}
         autoComplete="off"
+        variant="outlined"
+        label="Name:"
+        required
       />
-      <label htmlFor="desc">
-        <p>Description:</p>
-      </label>
-      <p>Add a description! (optional)</p>
-      <input
-        className="garden-desc"
+      <Typography variant="overline">Add a description(optional)</Typography>
+      <TextField
         type="text"
         name="desc"
         onChange={descChangeHandler}
         value={desc}
         autoComplete="off"
+        variant="outlined"
+        label="Description:"
+        multiline
+        rows={3}
       />
-    </Container>
+    </Grid>
   );
 };
