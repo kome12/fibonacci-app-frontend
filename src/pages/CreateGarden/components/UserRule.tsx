2a1caac6ad001a52275db2a839f99d2cd088a24e
Conall Imlah <conallimlah@gmail.com>
2021-09-02 20:49:20 +0900

    use more material components

--- src/pages/CreateGarden/components/UserRule.tsx
+++ src/pages/CreateGarden/components/UserRule.tsx
@@ -1,12 +1,12 @@
-import { Box } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { NewUserRule } from "..";
 import "./UserRule.css";
 
 export const UserRule: React.FC<NewUserRule> = ({ name, description }) => {
   return (
-    <Box>
-      <h3>{name}</h3>
-      {description && <p>{description}</p>}
-    </Box>
+    <Grid container={true} direction="column" justifyContent="flex-start">
+      <Typography variant="h6">{name}</Typography>
+      {description && <Typography variant="body1">{description}</Typography>}
+    </Grid>
   );
 };
