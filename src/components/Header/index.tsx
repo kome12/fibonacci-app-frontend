f0d46ab70930e1a665effd64dd47fd075b43c14c
Daichi Hashimura <dhashi92@gmail.com>
2021-09-03 21:52:32 +0900

    Change hamburger with user image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { createStyles, makeStyles } from "@material-ui/core";
+import { Avatar, createStyles, makeStyles } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
@@ -8,7 +8,6 @@ import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
 import { useUserState } from "../../store/user/useUserState";
 import { logout } from "../SignIn/useFirebaseAuth";
-import { ReactComponent as Hamburger } from "./assets/hamburger.svg";
 import MyNiwaLogo from "./assets/myniwa.svg";
 import styles from "./Header.module.css";
 
@@ -16,6 +15,10 @@ const useStyles = makeStyles(() =>
   createStyles({
     root: {
       flexDirection: "row",
+    },
+    large: {
+      width: 45,
+      height: 45,
     },
   })
 );
@@ -40,10 +43,10 @@ export const Header = () => {
     history.push("/");
   }, []);
 
-  const linkHandler = (page: string) => {
-    history.push(page);
-    handleClose();
-  };
+  // const linkHandler = (page: string) => {
+  //   history.push(page);
+  //   handleClose();
+  // };
 
   const classes = useStyles();
 
@@ -53,44 +56,39 @@ export const Header = () => {
       className={styles.header}
       classes={{ root: classes.root }}
     >
-      <Link to="/">
+      <Link to="/" className={styles.logoWrapper}>
         <img className={styles.logo} src={MyNiwaLogo} alt="my niwa logo" />
       </Link>
 
-      <Button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <div style={{ color: "#000000" }}>
-          <Hamburger />
-        </div>
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem
-          className="menu-link"
-          onClick={() => linkHandler("/user/myGardens")}
-        >
-          My Gardens
-        </MenuItem>
-        <MenuItem
-          className="menu-link"
-          onClick={() => linkHandler("/user/createGarden")}
-        >
-          Create Garden
-        </MenuItem>
-        {userData.isLoggedIn && (
-          <MenuItem className="menu-link" onClick={handleLogout}>
-            Logout
-          </MenuItem>
-        )}
-      </Menu>
+      {userData.isLoggedIn && (
+        <>
+          <Button
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <div className={styles.avatar}>
+              <Avatar
+                alt={userData.displayName}
+                src={userData.imageUrl ?? ""}
+                className={classes.large}
+              />
+              <span />
+            </div>
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem className="menu-link" onClick={handleLogout}>
+              Logout
+            </MenuItem>
+          </Menu>
+        </>
+      )}
     </AppBar>
   );
 };
