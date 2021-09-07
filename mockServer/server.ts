947a9ab84e3f7d4e6131fcf2cc46fbc1f805c7e5
Daichi Hashimura <dhashi92@gmail.com>
2021-09-07 23:03:23 +0900

    Remove temp mock data

--- mockServer/server.ts
+++ mockServer/server.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import express, { Express } from "express";
 import gardenRouter from "./routes/garden";
 import gardensRouter from "./routes/gardens";
-import tmpRouter from "./routes/tmp";
 
 const app: Express = express();
 
@@ -10,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/tmp", tmpRouter);
 app.use("/api/v1/gardens", gardenRouter);
 app.use("/api/v1/gardens/userid", gardensRouter);
 
