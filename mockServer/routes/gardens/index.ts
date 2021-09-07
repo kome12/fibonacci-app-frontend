bdf6aafb944c151ae2c9a8141cabe48e81c29cf5
Daichi Hashimura <dhashi92@gmail.com>
2021-09-07 21:53:29 +0900

    Fix gardens mock data

--- mockServer/routes/gardens/index.ts
+++ mockServer/routes/gardens/index.ts
@@ -3,8 +3,10 @@ import { gardensData } from "../../data/gardensData";
 
 const router = express.Router();
 
-router.get("/", (_req: Request, res: Response) => {
-  res.status(200).json(gardensData);
+router.get("/:id", (_req: Request, res: Response) => {
+  setTimeout(() => {
+    res.status(200).json(gardensData);
+  }, 1000);
 });
 
 export default router;
