5244f1a51462b4723eeb2c4468283ec837fb469b
Daichi Hashimura <dhashi92@gmail.com>
2021-09-09 03:01:08 +0900

    Add mock data

---
+++ mockServer/data/tasks/completedTask.ts
@@ -0,0 +1,6 @@
+export const completedTaskData = {
+  fireBaseUserId: "61376d22b4d3c1b26125f02e", // string
+  ruleId: "6126360b8379320a7c60f970", // ObjectId
+  date: new Date().toISOString(), // date (in GMT)
+  rewardTypeId: "61274429d20570644762b99b",
+};
--- mockServer/server.ts
+++ mockServer/server.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Express } from "express";
 import gardensRouter from "./routes/gardens";
 import rulesRouter from "./routes/rules";
+import tasksRouter from "./routes/tasks";
 import usersRouter from "./routes/users";
 
 const app: Express = express();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/gardens", gardensRouter);
 app.use("/api/v1/rules", rulesRouter);
 app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/completedTasks", tasksRouter);
 
 app.listen(3001, () => {
   console.log("Start on port http://localhost:3001");
