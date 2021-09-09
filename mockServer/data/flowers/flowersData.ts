62caa51d7967bc367d7ea1a7917ba81d273681ce
Daichi Hashimura <dhashi92@gmail.com>
2021-09-10 02:30:45 +0900

    Add mock data

---
+++ mockServer/data/flowers/flowersData.ts
@@ -0,0 +1,191 @@
+export const flowersData = [
+  {
+    _id: "7128d52b4bd76bf2e3a75519",
+    name: "Flowering dogwood",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/flowering_dogwood_wnbw7d.gif",
+    price: 15,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a4",
+    name: "Marigold",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103161/icons/marigold_timpbu.gif",
+    price: 4,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a1",
+    name: "Dahlia",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/dahlia_jemyso.gif",
+    price: 7,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75518",
+    name: "Chamomile",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/cherry_blossom_fvhcq5.gif",
+    price: 10,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75516",
+    name: "Cosmos",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/cosmos_isdopf.gif",
+    price: 4,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75515",
+    name: "Begonia",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103159/icons/begonia_nz6tuy.gif",
+    price: 8,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75511",
+    name: "Iris",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1630492453/icons/iris_przxuc.gif",
+    price: 5,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75517",
+    name: "Cherry blossom",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/cherry_blossom_fvhcq5.gif",
+    price: 20,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "8128d52b4bd76bf2e3a755a0",
+    name: "sunflower",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103161/icons/sunflower_eaf66x.gif",
+    price: 5,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75513",
+    name: "Balloon flower",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103159/icons/balloon_flower_ut70yg.gif",
+    price: 2,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "612789b765c2e6a7e7e76bd3",
+    name: "Anemone",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1630492426/icons/anemone_egth6w.gif",
+    price: 3,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a5",
+    name: "Lily",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/lily_s6luhq.gif",
+    price: 8,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a8",
+    name: "Peony",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103161/icons/peony_bj2gif.gif",
+    price: 20,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75512",
+    name: "Morning glory",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631101856/icons/morning_glory_dlevcp.gif",
+    price: 1,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a3",
+    name: "Lotus",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/lotus_o413qr.gif",
+    price: 12,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a2",
+    name: "Garden pansy",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103160/icons/garden_pansy_k3qqzl.gif",
+    price: 3,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a75514",
+    name: "Carnation",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103159/icons/carnation_jn3j8u.gif",
+    price: 7,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a7",
+    name: "Rose",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631105007/icons/rose_xkqhcn.gif",
+    price: 6,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "2128d52b4bd76bf2e3a755a9",
+    name: "Gerbera",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103161/icons/gerbera_ilcjh7.gif",
+    price: 7,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "7128d52b4bd76bf2e3a755a6",
+    name: "Lupine",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1631103161/icons/lupine_wmcy97.gif",
+    price: 3,
+    isActive: true,
+    isSecret: false,
+  },
+  {
+    _id: "6128d52b4bd76bf2e3a75510",
+    name: "Turnip",
+    imageURL:
+      "https://res.cloudinary.com/dyvrke6ml/image/upload/v1630492442/icons/tulip_r84l50.gif",
+    price: 5,
+    isActive: true,
+    isSecret: false,
+  },
+];
