9de7f1325f497887c1193d1b1a95ba2a8a9741a2
Curtis Huang <48991824+curtisahuang@users.noreply.github.com>
2021-09-09 23:18:34 +0800

    shows categories when making flower bed

--- src/pages/CreateGarden/components/NewGarden.tsx
+++ src/pages/CreateGarden/components/NewGarden.tsx
@@ -8,12 +8,23 @@ import {
 } from "@material-ui/core";
 import { motion } from "framer-motion";
 import React from "react";
+import { useMemo, useEffect } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { getCategories } from "../../../helpers/api/gardens/getCategories";
+import { useApi } from "../../../utils/api/useApi";
 
 interface NewGardenProps {
   nameChangeHandler: React.ChangeEventHandler<HTMLInputElement>;
   name: string;
   descChangeHandler: React.ChangeEventHandler<HTMLInputElement>;
   desc: string;
+  categoryChangeHandler: (
+    e: React.ChangeEvent<{ name?: string | undefined; value: unknown }>
+  ) => void;
+  category: string;
   animDirection: "left" | "right";
 }
 
@@ -40,6 +51,13 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "90%",
       marginLeft: "5%",
     },
+    formControl: {
+      margin: "auto",
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
   })
 );
 
@@ -48,11 +66,24 @@ export const NewGarden: React.FC<NewGardenProps> = ({
   name,
   descChangeHandler,
   desc,
+  categoryChangeHandler,
+  category,
   animDirection,
 }) => {
   const initDir = animDirection === "left" ? "5vw" : "-5vw";
   const exitDir = animDirection === "left" ? "-5vw" : "5vw";
   const classes = useStyles();
+
+  const [categoriesApi, getGardenCategories] = useApi(getCategories);
+
+  const categories = useMemo(() => categoriesApi.response, [categoriesApi]);
+
+  useEffect(() => {
+    console.log(categories);
+    getGardenCategories();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Grid
       container={true}
@@ -94,6 +125,24 @@ export const NewGarden: React.FC<NewGardenProps> = ({
         variant="outlined"
         label="Description:"
       />
+      <FormControl className={classes.formControl}>
+        <InputLabel id="category-select-label">Category</InputLabel>
+        <Select
+          labelId="category-select-label"
+          id="category-select"
+          value={category}
+          onChange={categoryChangeHandler}
+          required
+        >
+          {categories?.map((category, index) => {
+            return (
+              <MenuItem value={category._id} key={index}>
+                {category.name}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
     </Grid>
   );
 };
