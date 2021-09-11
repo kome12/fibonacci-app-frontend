b98c156e5492209b5aa69f0e1736c54b36fdb816
Conall Imlah <conallimlah@gmail.com>
2021-09-11 23:54:48 +0900

    remove unnecessary more button check

--- src/components/UserViewLayout/index.tsx
+++ src/components/UserViewLayout/index.tsx
@@ -17,10 +17,8 @@ export const UserViewLayout: React.FC<UserViewLayoutProps> = ({
   const [currentPage, setCurrentPage] = useState("/user/myniwa");
   const history = useHistory();
   const handlePageChange = (_event, newValue: string) => {
-    if (newValue !== "more") {
       setCurrentPage(newValue);
       history.push(newValue);
-    }
   };
   return (
     <div className={styles.layout}>
