2e4e65e2a6cca893fc81f65cde34896a5184f395
Daichi Hashimura <dhashi92@gmail.com>
2021-09-14 00:21:03 +0900

    Add section component

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,10 @@
+import { Grid } from "@material-ui/core";
+import styles from "./Section.module.css";
+
+export const Section: React.FC = ({ children }) => {
+  return (
+    <Grid container component="section" className={styles.container}>
+      {children}
+    </Grid>
+  );
+};
