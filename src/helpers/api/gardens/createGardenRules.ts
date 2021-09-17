40f591e6cf5d48f51313d8f8dfe05d789477df45
Daichi Hashimura <dhashi92@gmail.com>
2021-09-17 20:16:30 +0900

    Add helper

--- src/helpers/api/gardens/createGardenRules.ts
+++ src/helpers/api/gardens/createGardenRules.ts
@@ -5,3 +5,6 @@ export type NewGardenRule = Pick<Rule, "name" | "description" | "gardenId">;
 
 export const createGardenRules = (data: NewGardenRule[]) =>
   api.post<Rule[]>("/rules/bulk", data);
+
+export const createGardenRule = (data: NewGardenRule) =>
+  api.post<Rule>("/rules", data);
